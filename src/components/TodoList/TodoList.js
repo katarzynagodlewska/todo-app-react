fd66fac4ed665298d6f37796400d0a4f74c31752
Kasia <katarzyna-godlewska1@wp.pl>
2020-01-30 23:07:55 +0100

    feat: add button, input and function to make todo elements

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,10 +2,32 @@ import React, { useState } from 'react';
 import Todo from './Todo';
 
 function TodoList(props) {
-    const [todoList, setTodoList] = useState(["Moje zadanie","moje zadanie2", "Moje zadanie3"])
+    const [todoList, setTodoList] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (event) => {
+        const {value} = event.target;
+        setInputValue(value);
+    }
+
+    const handleButtonClick = () => {
+        setTodoList([...todoList, inputValue]);
+        setInputValue("");
+    }
     return (
         <div>
-            Moja aplikacja Todo  
+            Moja aplikacja Todo 
+            <input
+                name="Todo Input"
+                placeholder="Co będziemy dzisiaj rbić?"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+            <button
+                onClick={handleButtonClick}
+            >
+                Dodaj
+            </button>
             {todoList.map((todo) => (            
             <Todo                 
             key={todo}
